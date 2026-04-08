402a84ca8c78ffe403ff911801b7d47d7f3eb623
Wyatt Chamberlin <wyattchamberlin6@gmail.com>
2026-04-08 13:25:33 -0400

    fixed navigation to product-page when View Documentation button was clicked

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
 				<div className={styles.buttons}>
 					<Link
 						className="button button--secondary button--lg"
-						to="/docs/category/product-page"
+						to="/docs/frontend"
 					>
 						View Documentation
 					</Link>
